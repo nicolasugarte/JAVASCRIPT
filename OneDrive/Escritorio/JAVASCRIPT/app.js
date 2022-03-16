f567f9a7af432211afdf7479c97e09d72b804a6b
Nicolas Ugarte <nicolas.ugarte@live.com.ar>
2022-03-16 14:14:37 -0300

    prueba java

---
+++ OneDrive/Escritorio/JAVASCRIPT/app.js
@@ -0,0 +1,14 @@
+let nombre = prompt("completa tu  primer nombre y apellido");
+alert("hola " + nombre + " te dire como te llaman");
+
+
+if (nombre = "marcos") {
+    alert("TE DICEN" + "gordo mufa");
+}
+else if (nombre = "martin"); {
+    alert("tu nombre es" + "ruso puto");
+}   
+
+
+
+
